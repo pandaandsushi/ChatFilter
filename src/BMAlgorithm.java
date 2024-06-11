0614b7cffd3818666b99ec43615e00b59cc2f2dd
Thea Josephine <theajosephine04@gmail.com>
2024-06-12 00:39:21 +0700

    feat(algo): add KMP and BM algo

---
+++ src/BMAlgorithm.java
@@ -0,0 +1,46 @@
+// BM ALGORITHM
+
+// function to build lastoccurence table
+// will return array of lasttoccur
+public class BMAlgorithm {
+    public static int[] lastoccur(String pattern){
+        System.out.println("Let's build the lasst occurence list!\n");
+        int lastoccur[] = new int[128];
+        for (int i = 0; i<128; i++){
+            lastoccur[i] = -1;  //every char doesnt exist at first
+        }
+        for (int i = 0; i<pattern.length(); i++){
+            lastoccur[pattern.charAt(i)] = i;
+        }
+        return lastoccur;
+    }
+    
+    // main algo of BM
+    public static int BMAlgo(String pattern, String text){
+        int n = text.length();
+        int m = pattern.length();
+        int lastoccur[] = lastoccur(pattern);
+        int idx_i = m-1;
+        int idx_j = m-1;
+        
+        while (idx_i<=n-1) {
+            if(pattern.charAt(idx_j)==text.charAt(idx_i)){
+                if(idx_j==0){
+                    System.out.println("Found it!");
+                    return idx_i;
+                }
+                else{
+                    idx_i--;
+                    idx_j--;
+                }
+            }
+            else{
+                int lo = lastoccur[text.charAt(idx_i)];
+                idx_i = idx_i+m-Math.min(idx_j, 1+lo);
+                idx_j = m-1;
+            }
+        }
+        System.out.println("We can't find the pattern");
+        return -1;
+    }
+}
---
+++ src/KMPAlgorithm.java
@@ -0,0 +1,61 @@
+// KMP ALGORITHM
+
+// function to build LPS / border func table in KMP  
+// will return an array of border func
+public class KMPAlgorithm {
+    // function to build LPS / border func table in KMP  
+    // will return an array of border func
+    public static int[] lpsValue(String pattern) {
+        System.out.println("Let's build the border function!\n");
+        int[] borderfunc = new int[pattern.length()];
+        int prefixbestlength = 0;
+        int idx = 1; // the lps array index starts from 1 since borderfunc[0] is always 0
+        borderfunc[0] = 0; // when j = 0 we always get border func 0
+        
+        while (idx < pattern.length()) {
+            if (pattern.charAt(idx) == pattern.charAt(prefixbestlength)) {
+                prefixbestlength++;
+                borderfunc[idx] = prefixbestlength;
+                idx++;
+            } else {
+                if (prefixbestlength > 0) { // based on matching prefix
+                    prefixbestlength = borderfunc[prefixbestlength - 1];
+                } else { // no match
+                    borderfunc[idx] = 0;
+                    idx++;
+                }
+            }
+        }
+        return borderfunc;
+    }
+
+    // main KMP algo that will return the idx found on text
+    public static int KMPAlgo(String pattern, String text) {
+        int n = text.length();
+        int m = pattern.length();
+        int[] borderfunc = lpsValue(pattern);
+        
+        // initialize starting mismatch index
+        int idx_i = 0;
+        int idx_j = 0;
+        
+        while (idx_i < n) {
+            if (pattern.charAt(idx_j) == text.charAt(idx_i)) {
+                if (idx_j == m - 1) {
+                    System.out.println("Found it!");
+                    return idx_i - m + 1;
+                }
+                idx_i++;
+                idx_j++;
+            } else {
+                if (idx_j > 0) {
+                    idx_j = borderfunc[idx_j - 1];
+                } else {
+                    idx_i++;
+                }
+            }
+        }
+        System.out.println("We can't find the pattern");
+        return -1;
+    }
+}
