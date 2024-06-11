ae0a0f35566f9e74daa10dac3e3ebdda185c214e
Thea Josephine <theajosephine04@gmail.com>
2024-06-12 01:04:43 +0700

    feat(algo): add Brute Force Algo

---
+++ src/BFAlgorithm.java
@@ -0,0 +1,22 @@
+public class BFAlgorithm {
+    public static int BFAlgo(String text, String pattern){
+        int n = text.length();
+        int m = pattern.length();
+        int idxfound = -1;
+        for (int i = 0; i<=n-m; i++){
+            int countsame = 0;
+            for (int j = 0; j<m; j++){
+                if (pattern.charAt(j)!=text.charAt(i+j)){
+                    break;
+                }    
+                countsame++;
+            }
+            if(countsame==m){
+                System.out.println("Found it!");
+                idxfound = i;
+                break;
+            }
+        }
+        return idxfound;
+    }
+}
